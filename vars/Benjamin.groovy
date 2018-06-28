ec97bb544bcef1b7d43bf7604bec64e5d5cea827
cwfr-bbeurier <cwfr-bbeurier@PAR117525N02.emea.cpwr.corp>
2018-06-28 12:28:32 +0200

    Test

--- vars/Benjamin.groovy
+++ vars/Benjamin.groovy
@@ -1,1 +1,33 @@
-Bonjour
+pipeline {
+  agent { label 'AgentOrange'}
+  parameters {
+        string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+    }
+  stages {
+    stage('Build') {
+      steps {
+        echo "Hello from $PERSON"
+        sleep 10
+        input message: 'On y va ?', ok: 'Oui'
+     
+     
+      }
+    }
+    stage('Test') {
+      steps {
+          echo 'Testing Testing 123'
+      }
+    }
+    stage('Integration Test') {
+      steps {
+          echo 'Integration Testing'
+      }
+    }
+    stage('Deploy') {
+      steps {
+        echo 'Deploy some things'
+        step([$class: 'CordellWalkerRecorder']) 
+      }
+    }
+  }
+}
