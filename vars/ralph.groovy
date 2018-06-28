98b98578d5b302aa2c3bff0fa2fff097edb8db6a
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-06-28 12:32:03 +0200

    Shared Lib Script

--- vars/ralph.groovy
+++ vars/ralph.groovy
@@ -1,25 +1,38 @@
-pipeline {
-  agent any
-  stages {
-    stage('Build') {
-      steps {
-        echo 'Hello from Build'
-      }
+def call() 
+{
+    pipeline 
+    {
+        agent any
+        stages 
+        {
+            stage('Build') 
+            {
+                steps 
+                {
+                    echo 'Hello from Build'
+                }
+            }
+            stage('Test') 
+            {
+                steps 
+                {
+                    echo 'Testing Testing 123'
+                }
+            }
+            stage('Integration Test') 
+            {
+                steps 
+                {
+                    echo 'Integration Testing Testing 666'
+                }
+            }
+            stage('Deploy') 
+            {
+                steps 
+                {
+                    echo 'Deploy some things'
+                }
+            }
+        }
     }
-    stage('Test') {
-      steps {
-          echo 'Testing Testing 123'
-      }
-    }
-    stage('Integration Test') {
-      steps {
-          echo 'Integration Testing Testing 666'
-      }
-    }
-    stage('Deploy') {
-      steps {
-        echo 'Deploy some things'
-      }
-    }
-  }
 }
