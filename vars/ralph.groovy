97673374427dbdcbb1739860e2417f0dccf346e6
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-06-28 12:25:27 +0200

    Script added

--- vars/ralph.groovy
+++ vars/ralph.groovy
@@ -1,2 +1,25 @@
-// New Script Changed
-// Yet another change
+pipeline {
+  agent any
+  stages {
+    stage('Build') {
+      steps {
+        echo 'Hello from Build'
+      }
+    }
+    stage('Test') {
+      steps {
+          echo 'Testing Testing 123'
+      }
+    }
+    stage('Integration Test') {
+      steps {
+          echo 'Integration Testing Testing 666'
+      }
+    }
+    stage('Deploy') {
+      steps {
+        echo 'Deploy some things'
+      }
+    }
+  }
+}
