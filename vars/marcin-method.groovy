cc9f1fffe9b99ee3c8f3b9d72f98f3aa449f9b70
mgrabinski <37374538+mgrabinski@users.noreply.github.com>
2018-06-28 02:54:20 -1200

    Update marcin-method.groovy

--- vars/marcin-method.groovy
+++ vars/marcin-method.groovy
@@ -1,4 +1,4 @@
 def call(Map pipelineParams) 
 {
-    echo "Marcin's method here. Hello, ${pipelineParams.firstname} ${pipelineParams.lastname}"
+    echo "Marcin's method here. Hello, $pipelineParams.firstname $pipelineParams.lastname"
 }
