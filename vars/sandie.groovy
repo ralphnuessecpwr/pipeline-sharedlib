b96e3a89e8e148fb72d73ea33b51d381057647c4
cwuk-semmerson <cwuk-semmerson@uk-094965n72.emea.cpwr.corp>
2018-06-28 12:18:08 +0100

    Sandie

---
+++ vars/sandie.groovy
@@ -0,0 +1,25 @@
+pipeline {
+  agent any
+  stages {
+    stage('Build') {
+      steps {
+        echo 'Hello from Build'
+      }
+    }
+    stage('Test') {
+      steps {
+          echo 'Testing Testing 123'
+      }
+	    }
+    stage('Integration Testing') {
+      steps {
+          echo 'Testing Testing integration'
+      }
+    }
+    stage('Deploy') {
+      steps {
+        echo 'Deploy some things'
+      }
+    }
+  }
+}
