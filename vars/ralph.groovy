26ad1e0c8a2958d4b0033fd05907b2e6161cadec
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-08-17 18:35:42 +0200

    Constants 2

--- vars/ralph.groovy
+++ vars/ralph.groovy
@@ -7,6 +7,16 @@ import groovy.json.JsonOutput
 import java.net.URL
 import com.compuware.devops.util.*
 
+String Git_Credentials      = "github"
+String Git_URL              = "https://github.com/${Git_Project}"
+String Git_TTT_Repo         = "${ISPW_Stream}_${ISPW_Application}_Unit_Tests.git"
+String Git_Branch           = "master"
+String SQ_Scanner_Name      = "scanner" 
+String SQ_Server_Name       = "localhost"  
+String MF_Source            = "MF_Source"
+String XLR_Template         = "A Release from Jenkins"
+String XLR_User	            = "admin"	
+
 def call(Map pipelineParams)
 {
     node
@@ -14,17 +24,7 @@ def call(Map pipelineParams)
         println "Params " + pipelineParams.firstname
         Helper helper = new Helper(this)
         helper.helloWorld(pipelineParams.firstname)
-    
-        String Git_Credentials      = "github"
-        String Git_URL              = "https://github.com/${Git_Project}"
-        String Git_TTT_Repo         = "${ISPW_Stream}_${ISPW_Application}_Unit_Tests.git"
-        String Git_Branch           = "master"
-        String SQ_Scanner_Name      = "scanner" 
-        String SQ_Server_Name       = "localhost"  
-        String MF_Source            = "MF_Source"
-        String XLR_Template         = "A Release from Jenkins"
-        String XLR_User	            = "admin"	
-    
+        
        println "XLR_User " + XLR_User 
     }
 }
