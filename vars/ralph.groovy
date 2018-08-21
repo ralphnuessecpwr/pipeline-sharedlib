8dba3208c793fc5bc8ca0d8f22d026547f93a9e0
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-08-21 14:27:08 +0200

    Test

--- vars/ralph.groovy
+++ vars/ralph.groovy
@@ -7,31 +7,36 @@ import groovy.json.JsonOutput
 import java.net.URL
 import com.compuware.devops.util.*
 
+String Git_Credentials      = "87763671-db9a-47e1-80e7-33c1aba803b1"
+String Git_URL              
+String Git_TTT_Repo         
+String Git_Branch           = "master"
+String SQ_Scanner_Name      = "scanner" 
+String SQ_Server_Name       = "localhost"  
+String MF_Source            = "MF_Source"
+String XLR_Template         = "A Release from Jenkins"
+String XLR_User	            = "admin"	
+
+def String getPathNum(String Level)
+{
+    return Level.charAt(Level.length() - 1)
+}
+
 def call(Map pipelineParams)
 {
     node
     {
-        PropertiesInfo propertiesInfo = new PropertiesInfo(pipelineParams)
+
+        Git_URL             = "https://github.com/${proprtiesInfo.Git_Project}"
+        Git_TTT_Repo        = "${propertiesInfo.ISPW_Stream}_${propertiesInfo.ISPW_Application}_Unit_Tests.git"
+
+        def pathNum         = getPathNum(pipelineParams.ISPW_Level)
+
         Helper helper = new Helper(this)
 
-        echo "Git_URL: " + propertiesInfo.Git_URL
-/*        
-        helper.echoValue("ISPW_Stream: " + pipelineParams.ISPW_Stream)
-        helper.echoValue("ISPW_Container: " + pipelineParams.ISPW_Container)
-        helper.echoValue("ISPW_Level: " + pipelineParams.ISPW_Level)
-        helper.echoValue("SetId: " + pipelineParams.SetId)
-        helper.echoValue("ISPW_Release: " + pipelineParams.ISPW_Level)
-        helper.echoValue("Owner: " + pipelineParams.Owner)
+        echo "Git_URL: " + Git_URL
+        echo "Git_TTT_Repo: " + Git_TTT_Repo
+        echo "pathNum: " + pathNum
 
-        def Git_Credentials      = "github"
-        def Git_URL              = "https://github.com/${Git_Project}"
-        def Git_TTT_Repo         = "${ISPW_Stream}_${ISPW_Application}_Unit_Tests.git"
-        def Git_Branch           = "master"
-        def SQ_Scanner_Name      = "scanner" 
-        def SQ_Server_Name       = "localhost"  
-        def MF_Source            = "MF_Source"
-        def XLR_Template         = "A Release from Jenkins"
-        def XLR_User	         = "admin"	
-*/
     }
 }
