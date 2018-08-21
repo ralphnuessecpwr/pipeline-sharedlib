d82764cccfe823b08308cd86f343d14e0adcf346
CWDE-RNUESSE <ralph.nuesse@compuware.com>
2018-08-21 15:01:12 +0200

    Next stages

--- vars/ralph.groovy
+++ vars/ralph.groovy
@@ -33,7 +33,7 @@ def gitcheckout(String URL, String Branch, String Credentials, String Folder)
     println "Scenario " + Branch
     println "Scenario " + Credentials
 
-    checkout changelog: false, poll: false, 
+    steps.checkout changelog: false, poll: false, 
     scm: [$class: 'GitSCM', 
     branches: [[name: "*/${Branch}"]], 
     doGenerateSubmoduleConfigurations: false, 
@@ -74,9 +74,86 @@ def call(Map pipelineParams)
                     credentialsId: "${pipelineParams.HCI_Token}",      
                     containerName: "${pipelineParams.SetId}",   
                     containerType: '2',                 // 0-Assignment 1-Release 2-Set
-                    ispwDownloadAll: false,             // false will not download files that exist in the workspace and haven't previous changed
+                    ispwDownloadAll: true,             // false will not download files that exist in the workspace and haven't previous changed
                     serverConfig: '',                   // ISPW runtime config.  if blank ISPW will use the default runtime config
                     serverLevel: ''])                   // level to download the components from
         }
+
+        stage("Retrieve Tests")
+        {
+            //Retrieve the Tests from Github that match that ISPWW Stream and Application
+            Git_URL = "${Git_URL}/${Git_TTT_Repo}"
+
+            //call gitcheckout wrapper function
+            gitcheckout(Git_URL, Git_Branch, Git_Credentials, "tests")
+        }
+
+        // findFiles method requires the "Pipeline Utilities Plugin"
+        // Get all testscenario files in the current workspace into an array
+        def TTTListOfScenarios = findFiles(glob: '**/*.testscenario')
+
+        // Get all Cobol Sources in the MF_Source folder into an array 
+        def ListOfSources  = findFiles(glob: "**/${ISPW_Application}/${MF_Source}/*.cbl")
+
+        // Define a empty array for the list of programs
+        def ListOfPrograms = []
+        
+        // Determine program names for each source member
+        ListOfSources.each
+        {
+            // The split method uses regex to search for patterns, therefore
+            // Backslashes, Dots and Underscores which mean certain patterns in regex need to be escaped 
+            // The backslash in Windows paths is duplicated in Java, therefore it need to be escaped twice
+            // Trim ./cbl from the Source members to populate the array of program names
+            ListOfPrograms.add(it.name.trim().split("\\.")[0])
+        }
+
+        /* 
+        This stage executes any Total Test Projects related to the mainframe source that was downloaded
+        */ 
+        stage("Execute related Unit Tests")
+        {
+            // Loop through all downloaded Topaz for Total Test scenarios
+            TTTListOfScenarios.each
+            {
+
+                // Get root node of the path, i.e. the name of the Total Test project
+                def TTTScenarioPath        = it.path // Fully qualified name of the Total Test Scenario file
+                def TTTProjectName         = it.path.trim().split("\\\\")[0] + "\\"+ it.path.trim().split("\\\\")[1]  // Total Test Project name is the root folder of the full path to the testscenario 
+                def TTTScenarioFullName    = it.name  // Get the full name of the testscenario file i.e. "name.testscenario"
+                def TTTScenarioName        = it.name.trim().split("\\.")[0]  // Get the name of the scenario file without ".testscenario"
+                def TTTScenarioTarget      = TTTScenarioName.split("\\_")[0]  // Target Program will be the first part of the scenario name (convention)
+        
+                // For each of the scenarios walk through the list of source files and determine if the target matches one of the programs
+                // In that case, execute the unit test.  Determine if the program name matches the target of the Total Test scenario
+                if(ListOfPrograms.contains(TTTScenarioTarget))
+                {
+                    // Log which 
+                    println "*************************"
+                    println "Scenario " + TTTScenarioFullName
+                    println "Path " + TTTScenarioPath
+                    println "Project " + TTTProjectName
+                    println "*************************"
+                
+                    steps.step([$class: 'TotalTestBuilder', 
+                        ccClearStats: false,                // Clear out any existing Code Coverage stats for the given ccSystem and ccTestId
+                        ccRepo: "${CC_repository}",
+                        ccSystem: "${ISPW_Application}", 
+                        ccTestId: "${BUILD_NUMBER}",        // Jenkins environment variable, resolves to build number, i.e. #177 
+                        credentialsId: "${HCI_Token}", 
+                        deleteTemp: true,                   // (true|false) Automatically delete any temp files created during the execution
+                        hlq: '',                            // Optional - high level qualifier used when allocation datasets
+                        connectionId: "${HCI_Conn_ID}",    
+                        jcl: "${TTT_Jcl}",                  // Name of the JCL file in the Total Test Project to execute
+                        projectFolder: "${TTTProjectName}", // Name of the Folder in the file system that contains the Total Test Project.  
+                        testSuite: "${TTTScenarioFullName}",// Name of the Total Test Scenario to execute
+                        useStubs: false])                   // (true|false) - Execute with or without stubs
+                }
+            }
+
+            // Process the Total Test Junit result files into Jenkins
+            steps.junit allowEmptyResults: true, keepLongStdio: true, testResults: "TTTUnit/*.xml"
+        }
+
     }
 }
